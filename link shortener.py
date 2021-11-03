e7c1af5d0989c4cda48d3f988b1835beaa4a8c06
Coding Masters-YT <codingsforlife@gmail.com>
2021-11-03 18:18:10 +0530

    Add files via upload

---
+++ link shortener.py
@@ -0,0 +1,12 @@
+﻿import pyshorteners
+
+
+while True:
+
+  link = input("Enter the link or type q to exit: ")
+  if link == "q":
+    break
+
+  shortener = pyshorteners.Shortener()
+
+  print(shortener.tinyurl.short(link))
